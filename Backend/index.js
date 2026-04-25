c3ba6b2f63aee599c3faedc53212f1a4b16d62f8
piyushSingh5009 <piyushsingh5009@gmail.com>
2026-04-25 07:06:59 +0530

    Replace manual chatbot logic with Gemini API integration

--- Backend/index.js
+++ Backend/index.js
@@ -4,20 +4,22 @@ import mongoose from 'mongoose';
 import chatbotRoutes from './routes/chatbot.routes.js';
 import cors from 'cors';
 
-const app = express()
-dotenv.config()
+const app = express();
+dotenv.config();
+
 const port = process.env.PORT || 3000
 
 app.use(express.json());
 app.use(cors());
+
+//defining routes
+app.use("/bot/v1/", chatbotRoutes)
 
 mongoose.connect(process.env.MONGO_URI)
 .then(()=>{
     console.log("Connected to MongoDB")
 })
 .catch(err=>console.log("Error connecting to mongoDB",err))
-//defining routes
-app.use("/bot/v1/", chatbotRoutes)
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`)
