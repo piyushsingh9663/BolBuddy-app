b7b29c4458f7baf79fdebbd28608f37350d9b085
piyushSingh5009 <piyushsingh5009@gmail.com>
2026-05-02 10:53:49 +0530

    Improving email domain validation

--- Backend/routes/auth.routes.js
+++ Backend/routes/auth.routes.js
@@ -2,11 +2,27 @@ import express from "express";
 import { register,login,deleteMe } from "../controllers/auth.controller.js";
 import { protect } from "../middleware/auth.middleware.js";
 import {body,validationResult} from "express-validator";
+import dns from 'dns/promises';
 
 const router=express.Router();
 
 const validateRegister=[
-    body("email").isEmail().withMessage("Invalid email"),
+    body("email")
+    .isEmail().withMessage("Invalid email")
+    .normalizeEmail()
+    .custom(async(email)=>{
+        const domain=email.split("@")[1];
+        try{
+          const records=await dns.resolveMx(domain);
+          if(!records || records.length===0){
+            throw new Error("Email domain can't receive mail")
+          }
+        }catch(err){
+            throw new Error("Invalid email domain");
+        }
+        return true;
+
+    }),
     body("password").isLength({min:6}).withMessage("Min 6 chars")
 ];
 
